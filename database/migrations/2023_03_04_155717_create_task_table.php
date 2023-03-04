b30f78303ae57162aba72026b5914ce327b834b9
priyankasurti2389 <opportunitiespriyanka@gmail.com>
2023-03-04 23:20:40 +0530

    Create models

---
+++ database/migrations/2023_03_04_155717_create_task_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('description');
+            $table->string('status');
+            $table->uuid('project_id');
+            $table->uuid('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
